de3dbb274b176eb28927a1589bd8be2a8fdc25fc
Elir25 <elirendiles@gmail.com>
2021-06-21 11:30:40 +0200

    inclass exercise 1 cyf-hotel

--- cyf_hotels.sql
+++ cyf_hotels.sql
@@ -1,3 +1,11 @@
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS bank_details;
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS hotels CASCADE;
+DROP TABLE IF EXISTS rooms;
+
+
+---creating the tables---
 CREATE TABLE customers (
   id        SERIAL PRIMARY KEY,
   name      VARCHAR(30) NOT NULL,
@@ -8,6 +16,22 @@ CREATE TABLE customers (
   country   VARCHAR(20)
 );
 
+CREATE table hotels (
+hotel_id        SERIAL PRIMARY KEY,
+hotel_name        VARCHAR(30) NOT NULL,
+city              VARCHAR(15),
+country           VARCHAR(15) NOT NULL
+);
+
+CREATE table rooms (
+room_id        SERIAL PRIMARY KEY,
+--room_number         INT NOT NULL,--
+price_per_night     VARCHAR(10),
+hotel_id            INT REFERENCES hotels(hotel_id),
+type_of_room        VARCHAR(30)
+);
+
+
 create table bank_details (
 id        SERIAL PRIMARY KEY,
 customer_id INT REFERENCES customers(id),
@@ -15,32 +39,20 @@ bank_name   VARCHAR(38) NOT NULL,
 iban_code   VARCHAR(30) NOT NULL
 );
 
-CREATE table hotels (
-hotel_id        SERIAL PRIMARY KEY,
-hotel_name        VARCHAR(30) NOT NULL,
-street            VARCHAR(30) NOT NULL,
-number_of_street          INT NOT NULL,
-city              VARCHAR(15),
-country           VARCHAR(15) NOT NULL,
-postal_code       INT         NOT NULL,
-number_of_rooms   INT
-);
-
-CREATE table rooms (
-room_id        SERIAL PRIMARY KEY,
-room_number         INT NOT NULL,
-price_per_night     VARCHAR(10),
-hotel_id            INT REFERENCES hotels(hotel_id),
-type_of_room        VARCHAR(30)
-);
 
 CREATE table bookings (
 bookings_id        SERIAL PRIMARY KEY,
 room_id         INT REFERENCES rooms(room_id),
 customer_id     INT REFERENCES customers(id),
-check_in_date   DATE,
+check_in_date   DATE not NULL,
 number_of_nights   INT
 );
 
 INSERT into customers ( name, email, address, city, postcode, country ) values ( 'Elialba', 'eli@gmail.com', 'artesania street', 'Barcelona', '19023', 'Spain' );
-INSERT into hotels (hotel_name, street, number_of_street, city,) values ( 'Triple Point Hotel', 'marbella', '169', 'barcelona' );
+INSERT into hotels (hotel_name, city, country) values ( 'Triple Point Hotel', 'barcelona', 'Spain' );
+INSERT into hotels (hotel_name, city, country) values ( 'Royal Cosmos Hotel', 'barcelona', 'Spain' );
+INSERT into hotels (hotel_name, city, country) values ( 'Pacific Petal Motel', 'barcelona', 'Spain' );
+
+select * from customers;
+select * from hotels;
+
